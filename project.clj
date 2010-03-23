83b15d49e44ded3b1ec643e6c838d0563144dba8
Heinz N. Gies <heinz@licenser.net>
2010-03-24 00:55:16 +0100

    Updated project.clj

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :dependencies [
   [compojure "0.3.2"]
   [clj-sandbox "0.2.5-SNAPSHOT"]
-  [org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
-  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
+  [org.clojure/clojure "1.1.0"]
+  [org.clojure/clojure-contrib "1.1.0"]]
   :dev-dependencies [
   [swank-clojure/swank-clojure "1.1.0"]])
