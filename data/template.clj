3e9857c9d8732250997bd8c9a7717de4c8ed62df
clicki <clicki@clicki.(none)>
2010-03-24 16:53:31 +0100

    * Updated the default layout

--- data/template.clj
+++ data/template.clj
@@ -1,5 +1,8 @@
+(defn layout-with-head [uri head & body]
+  (api/html [:html [:head head] [:body body [:hr] [:p [:a {:href (str uri "?edit=")} "edit"] " | " [:a {:href "/index"} "home"]]]]))
+
 (defn layout [uri title & body]
-  (api/html [:html [:head [:title (str "Clicki - " title)]] [:body body [:p [:a {:href (str uri "?edit=")} "edit"] " | " [:a {:href "/index"} "home"]]]]))
+  (layout-with-head uri [:title (str "Clicki - " title)] body))
 
 (layout uri
   "Layout"
